768168d60fe8d7f2a4bd050771da501b693c3676
Robert Butler <r7butler@yahoo.com>
2022-05-06 12:00:30 -0700

    added arcgis engine configuration

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
+import json
 from sqlalchemy import create_engine
 
 from rain import sync_rain
 from functions import send_mail
 from watervolume import sync_watervolume
 from meta import sync_metadata
+from views import create_views
+
+from arcgis.gis import GIS
 
 DB_PLATFORM = os.environ.get('DB_PLATFORM')
 DB_HOST = os.environ.get('DB_HOST')
@@ -16,6 +20,24 @@ DB_NAME = os.environ.get('DB_NAME')
 eng = create_engine(
     f"{DB_PLATFORM}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
+
+### Arcgis login ###
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+config_path = "/".join([ROOT_DIR, 'config.json'])
+
+# READ JSON CONFIG FILE
+with open(config_path) as config_file:
+    config = json.load(config_file)
+    config = config['arc_gis']
+
+gis_username = config['GIS_USERNAME']
+gis_password = config['GIS_PASSWORD']
+gis = GIS("https://www.arcgis.com",gis_username,gis_password)
+print("Logged in as " + str(gis.properties.user.username))
+
+db_list = {
+	'sdturf1':'e77fe3bb3d424924ba50279ef90228b6'
+}
 
 # initialize report
 report = []
@@ -61,7 +83,8 @@ report = [
         url = os.environ.get('SHAREPOINT_SITE_URL'),
         teamname = 'SanDiegoCountyBMPMonitoring',
         sitefolder = 'Shared%20Documents/Turf%20Replacement/Data/Raw'
-    )
+    ),
+    *create_views(eng)
 ]
 
 SEND_FROM = 'admin@checker.sccwrp.org'
